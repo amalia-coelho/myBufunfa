83bc35163c13d0ea7fd7911adf290174ac07177a
Amália Coelho <apenas.coelho03@gmail.com>
2024-08-29 22:38:54 -0300

    Navbar básico

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Navbar from './components/Navbar.jsx'
-import './index.css'
+
+import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
