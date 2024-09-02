1614cf882000677e69b3a80311eedb06ec52029a
Amália Coelho <apenas.coelho03@gmail.com>
2024-09-01 22:19:28 -0300

    Navbar circular e itens posicionados

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,41 +4,38 @@ import { faHouse, faSquareCheck, faCalendarDays, faBullseye, faBars, faXmark} fr
 import '../styles/Navbar.css';
 
 const Navbar = () => {
-    const [isExpanded, setIsExpanded] = useState(false);
-
-    const toggleNavbar = () => {
-        setIsExpanded(!isExpanded);
+    const NavOption = ({ icon, label, angle }) => {
+        const elementStyle = {
+          transform: `rotate(${angle}deg) translate(350px) rotate(-${angle}deg)`,
+        };
+    
+        return (
+          <div className="nav-option" style={elementStyle}>
+            <a className="nav-item" href="#">
+              <FontAwesomeIcon icon={icon} />
+              <span>{label}</span>
+            </a>
+          </div>
+        );
     };
 
+    const icons = [faHouse, faSquareCheck, faCalendarDays, faBullseye, faBars];
+    const titles = ['Inicial', 'Tarefas', 'Calendário', 'Objetivos', 'Menu'];
+    const angles = [70, 30, 0, 320, 280];
 
     return (
-            <nav className={`navbar ${isExpanded ? 'expanded' : 'collapseds'}`}>
-                <div className="nav-header">
-                    <div className="nav-toggle">
-                        <button className="nav-toggle-button" onClick={toggleNavbar}>
-                        <FontAwesomeIcon icon={isExpanded ? faXmark : faBars} />
-                        </button>    
-                    </div>
-                    <div className="nav-routes">
-                        <a className="nav-item" href="#">
-                            <FontAwesomeIcon icon={faHouse} />
-                            {isExpanded && <span>Inicial</span>}
-                        </a>
-                        <a className="nav-item" href="#">
-                            <FontAwesomeIcon icon={faSquareCheck} />
-                            {isExpanded && <span>Tarefas</span>}
-                        </a>    
-                        <a className="nav-item" href="#">
-                            <FontAwesomeIcon icon={faCalendarDays} />
-                            {isExpanded && <span>Hábitos</span>}
-                        </a>
-                        <a className="nav-item" href="#">
-                            <FontAwesomeIcon icon={faBullseye} />
-                            {isExpanded && <span>Metas</span>}
-                        </a>
-                    </div>
-                </div>
+        <div className="nav-container">
+            <nav>
+                {icons.map((icon, index) => (
+                    <NavOption
+                        key={index}
+                        icon={icon}
+                        label={titles[index]}
+                        angle={angles[index]}
+                    />
+                ))}
             </nav>
+        </div>
     );
 }
 
