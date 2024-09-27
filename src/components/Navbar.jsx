284900ec700b2d17156839f67dabefffd4d6a95d
Amália Coelho <apenas.coelho03@gmail.com>
2024-09-27 20:32:05 -0300

    Navbar minimal v1 - aberto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,29 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserNinja, faGear, faDollarSign, faHouse, faSquareCheck, faCalendarDays, faBullseye, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faUserNinja, faGear, faXmark, faUserGroup, faDollarSign, faSquareCheck, faCalendarDays, faBullseye, faBars } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
     return (
         <div className='background-navbar'>
             <div className="nav-header">
-                <FontAwesomeIcon icon={faDollarSign} />
-                <span className="navbar-title">MyBufunfa</span>
+                <div className="app-logo">
+                    <FontAwesomeIcon icon={faDollarSign} className="navbar-icon"/>
+                    <span className="navbar-title">MyBufunfa</span>
+                </div>
+                <button className="btn-action btn-close-modal">
+                    <FontAwesomeIcon icon={faXmark}/>
+                </button>
             </div>
             <div className="nav-options">
-                <NavItem icon={faBars} title="Teste" />
-                <NavItem icon={faHouse} title="Teste" />
-                <NavItem icon={faSquareCheck} title="Teste" />
-                <NavItem icon={faBullseye} title="Teste" />
-                <NavItem icon={faBars} title="Teste" />
+                <NavItem icon={faBars} title="Página inicial" />
+                <NavItem icon={faSquareCheck} title="Planejamento" />
+                <NavItem icon={faBullseye} title="Relatórios" />
+                <NavItem icon={faUserGroup} title="Amigos" />
             </div>
             <div className="nav-footer">
                 {/* <img src=".../assets/user/default-avatar.jpg"   onError={(e) => { e.target.src = "../assets/user/default-avatar.jpg";}} /> */}
                 <FontAwesomeIcon icon={faUserNinja} className="nav-footer-item img-user-sm"/>
                 <span>Amália Coelho</span>
-                <FontAwesomeIcon icon={faGear} />
             </div>
         </div>
     );
@@ -29,7 +32,7 @@ const Navbar = () => {
 const NavItem = (props) => {
     return (
         <div className='nav-item'>
-            <FontAwesomeIcon icon={props.icon} />
+            <FontAwesomeIcon icon={props.icon} className="nav-item-icon"/>
             <span className='nav-item-title'>{props.title}</span>
         </div>
     );
