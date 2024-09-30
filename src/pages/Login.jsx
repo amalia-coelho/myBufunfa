5ee095e3ab569497fba8eba070b2ce57c7935d12
Amália Coelho <apenas.coelho03@gmail.com>
2024-09-29 21:37:36 -0300

    Novas páginas

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import '../styles/Login.css';
+
+export default function Login() {
+    return (
+        <div className="container">
+            <h1 className="title">$ MyBufunfa</h1>
+            <input></input>
+            <button>Entrar</button>
+        </div>
+    );
+}
