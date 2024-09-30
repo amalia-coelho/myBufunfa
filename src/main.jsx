2d7c19eab8329e850afcb41b9e74d6d6b5e7c524
Amália Coelho <apenas.coelho03@gmail.com>
2024-09-29 21:02:30 -0300

    Implementação do react route

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,34 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Navbar from "./components/Navbar";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Navbar from './components/Navbar.jsx'
 
 import './styles/index.css'
+
+// Páginas
+import Home from "./pages/Home";
+import Cadastro from "./pages/Cadastro";
+import Login from "./pages/Login";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/cadastro",
+    element: <Cadastro />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
